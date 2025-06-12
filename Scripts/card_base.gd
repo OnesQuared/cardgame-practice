1a8ee40694f47499acbec4a0c92f0b7f9c80f10d
Paul Moon <bakamono.moon@gmail.com>
2025-06-12 10:10:37 -0400

    card text
    card text now updates and fills the labels

--- Scripts/card_base.gd
+++ Scripts/card_base.gd
@@ -1,6 +1,15 @@
-extends Node2D
+class_name Card extends Node2D
 
-@export var CardName: String
-@export var CardDescription: String
-@export var CardCost: int
+@export var CardName: String = "Name"
+@export var CardDescription: String = "Description"
+@export var CardCost: int = 1
 @export var CardImage: Node2D
+
+@onready var CostLabel: Label = $CardCost/CostLabel
+@onready var NameLabel: Label = $CardName/Label
+@onready var DescLabel: Label = $CardDescription
+
+func _ready():
+	CostLabel.set_text(str(CardCost))
+	NameLabel.set_text(CardName)
+	DescLabel.set_text(CardDescription)
