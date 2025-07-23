28de1be727ba7c1606f1c18fb7b94a18acfe6901
Paul Moon <bakamono.moon@gmail.com>
2025-07-23 15:04:46 -0400

    support type test
    added support type to test out later

--- Scripts/handScript.gd
+++ Scripts/handScript.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var cardpoint = $CanvasLayer/HandPoint
 @onready var offence_card: PackedScene = preload("res://Scene/offence.tscn")
 @onready var defence_card: PackedScene = preload("res://Scene/defence.tscn")
+@onready var support_card: PackedScene = preload("res://Scene/support.tscn")
 
 func _on_button_pressed():
 	var offencecard = offence_card.instantiate()
@@ -12,3 +13,8 @@ func _on_button_pressed():
 func _on_button_2_pressed() -> void:
 	var defencecard = defence_card.instantiate()
 	cardpoint.add_child(defencecard)
+
+
+func _on_button_3_pressed() -> void:
+	var supportcard = support_card.instantiate()
+	cardpoint.add_child(supportcard)
